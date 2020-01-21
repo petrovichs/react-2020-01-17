7754ba78f83cb7dd18ccbdee1789e57ab611e4b8
Maksym Kramarenko <maksym@wix.com>
2020-01-21 20:58:14 +0200

    Add custom hooks

---
+++ src/custom-hooks/use-amount.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+export const useAmount = (initialValue = 0) => {
+  const [amount, setAmount] = useState(initialValue)
+  const decrease = () => {
+    setAmount(amount < 0 ? amount - 1 : 0)
+  }
+  const increase = () => {
+    setAmount(amount + 1)
+  }
+  return {
+    amount,
+    decrease,
+    increase,
+  }
+}
